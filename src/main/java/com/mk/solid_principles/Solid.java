09723afa11330a0dcdc8a11cac3a63a87cc7b102
Murali Krishnan <muralikrishnan.pro@gmail.com>
2023-08-26 23:54:25 +0530

    single responsibility principle

---
+++ src/main/java/com/mk/solid_principles/Solid.java
@@ -0,0 +1,19 @@
+package com.mk.solid_principles;
+
+public class Solid {
+	/*
+	 * SOLID principles
+	 * S --> Single Responsibility Principle
+	 * O --> Open Closed Principle
+	 * L --> Liskov Subtitution Principle
+	 * I --> Interface Segregation Priciple
+	 * D --> Dependancy Inversion Priciple
+	 * 
+	 * Single Responsibility Principle
+	 * There should never be more than one reason for a class to change.
+	 * We can fullfill or satisfy this principle, only if our class is
+	 * providing a single specific and focused functionality.
+	 * 
+	 * 
+	 */
+}
